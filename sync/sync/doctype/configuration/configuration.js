1878b568ee47f825f79123aeae60ca2afbeb2d34
ramjanalilal <ramjanalilal143@gmail.com>
2025-06-02 13:36:19 +0000

    Deleted Document

--- sync/sync/doctype/configuration/configuration.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, RamjanAli Lal and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Configuration", {
-// 	refresh(frm) {
-
-// 	},
-// });
