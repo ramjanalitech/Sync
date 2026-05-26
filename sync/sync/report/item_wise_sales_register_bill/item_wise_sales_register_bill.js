82265de266ab216ab74ce03d79fe7583fea3ce94
ramjanalilal <ramjanalilal143@gmail.com>
2026-05-26 06:24:42 +0000

    item_wise_sales_register_bill

---
+++ sync/sync/report/item_wise_sales_register_bill/item_wise_sales_register_bill.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, RamjanAli Lal and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Item-wise Sales Register-Bill"] = {
+	"filters": [
+
+	]
+};
